e09afe35424504bdefee0d6ed05e471d37a68d30
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-05-11 14:55:42 -0300

    refactor: update to dynamic pipeline

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,12 +1,12 @@
 locals {
-  lambda_name        = var.project_name
-  lambda_handler     = "index.handler"
-  lambda_runtime     = "nodejs20.x"
-  lambda_log_group   = "/aws/lambda/${var.project_name}"
-  lambda_zip_key     = "${var.project_name}.zip"
+  lambda_name      = var.project_name
+  lambda_handler   = "index.handler"
+  lambda_runtime   = "nodejs20.x"
+  lambda_zip_key   = "${var.project_name}.zip"
+  lambda_log_group = "/aws/lambda/${var.project_name}"
 
-  queue_name         = "${var.project_name}-queue"
-  role_name          = "${var.project_name}_execution_role"
-  policy_name        = "${var.project_name}_policy"
-  bucket_name        = "meu-unico-bucket-s3" # pode parametrizar no futuro, se desejar
+  queue_name = "${var.project_name}-queue"
+
+  role_name   = "${var.project_name}_execution_role"
+  policy_name = "${var.project_name}_policy"
 }
