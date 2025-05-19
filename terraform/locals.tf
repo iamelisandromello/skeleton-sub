1d050c231fb1f139355f7fe706b2867c49bebfb1
Elisandro Mello Correa <elisandro@ulbra.edu.br>
2025-05-19 15:24:12 -0300

    refactor: update action to use template GitHub Actions

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,12 +1,28 @@
+#########################################
+# locals.tf (root module)
+#########################################
+
 locals {
-  project_name      = var.project_name
-  lambda_name       = "${local.project_name}-lambda"
-  queue_name        = "${local.project_name}-queue"
-  role_name         = "${local.project_name}-execution-role"
-  policy_name       = "${local.project_name}-lambda-policy"
-  bucket_name       = "meu-unico-bucket-s3"
-  lambda_zip_key    = "${local.project_name}.zip"
-  lambda_handler    = "index.handler"
-  lambda_runtime    = "nodejs20.x"
-  lambda_log_group  = "/aws/lambda/${local.lambda_name}"
+  # Identificadores padronizados
+  environment_suffix   = var.environment == "prod" ? "" : "-${var.environment}"
+
+  lambda_name          = "${var.project_name}${local.environment_suffix}"
+  lambda_role_name     = "${var.project_name}${local.environment_suffix}_execution_role"
+  logging_policy_name  = "${var.project_name}${local.environment_suffix}_logging_policy"
+  publish_policy_name  = "${var.project_name}${local.environment_suffix}-lambda-sqs-publish"
+
+  # Infraestrutura associada
+  log_group_name       = "/aws/lambda/${local.lambda_name}"
+  queue_name           = "${local.lambda_name}-queue"
+
+  s3_bucket_name       = var.s3_bucket_name
+  s3_object_key        = "${var.project_name}.zip"
+
+  merged_env_vars = merge(
+    var.global_env_vars,
+    var.environments[var.environment]
+  )
+
+  lambda_handler  = "main/app.handler"
+  lambda_runtime  = "nodejs20.x"
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,28 @@
-output "lambda_arn" {
-  value = aws_lambda_function.my_lambda_function.arn
-}
+#########################################
+# outputs.tf (root module)
+#########################################
 
-output "bucket_name" {
-  value = aws_s3_bucket.lambda_code_bucket.bucket
+output "lambda_arn" {
+  description = "ARN da função Lambda provisionada"
+  value       = module.lambda.lambda_arn
 }
 
 output "lambda_function_name" {
-  value = aws_lambda_function.my_lambda_function.function_name
+  description = "Nome da função Lambda provisionada"
+  value = module.lambda.lambda_function_name
+}
+
+output "bucket_name" {
+  description = "Nome do bucket S3 onde está o código da Lambda"
+  value       = data.aws_s3_bucket.lambda_code_bucket.bucket
+}
+
+output "sqs_queue_url" {
+  description = "URL da fila SQS associada à Lambda"
+  value       = module.sqs.queue_url
+}
+
+output "sqs_queue_arn" {
+  description = "ARN da fila SQS associada à Lambda"
+  value       = module.sqs.queue_arn
 }
